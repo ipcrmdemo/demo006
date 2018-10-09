64b5e8a0dc8f3f1ff48b37b40b2af33f22986335
Matt Cadorette <mcadorette@gmail.com>
2018-10-08 23:24:09 -0400

    Initial commit from Atomist

---
+++ src/main/java/demo006/Demo006Application.java
@@ -0,0 +1,12 @@
+package demo006;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo006Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo006Application.class, args);
+	}
+}
